ba7d7c2b8930ce7eded4c69ebef37c87f37809cf
JulianaOsi <julia.osintseva15@gmail.com>
2020-11-14 23:54:47 +0500

    Реализовано получение списка анализов для направления

--- cmd/medhelp.go
+++ cmd/medhelp.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-	"database/sql"
+	"context"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 
 	"github.com/JulianaOsi/medhelp/pkg/config"
 	migrations "github.com/JulianaOsi/medhelp/pkg/migration"
+	"github.com/JulianaOsi/medhelp/pkg/server"
+	"github.com/JulianaOsi/medhelp/pkg/store"
 )
 
 func main() {
 	conf := config.ReadConfig()
 
-	db, err := sql.Open("postgres", conf.DB.ToString())
+	err := migrations.UpMigrations(conf)
 	if err != nil {
-		log.Fatalf("goose: failed to open DB: %v\n", err)
+		log.Fatalf("failed to update migrations: %v\n", err)
 	}
 
-	migrations.Run(db)
+	s, err := store.New(conf.DB)
+	if err != nil {
+		log.Fatalf("failed to create store: %v\n", err)
+	}
+
+	analysis, err := s.GetAnalysisByDirectionId(context.Background(), "0")
+	if err != nil {
+		log.Fatalf("get analysis failed: %v\n", err)
+	}
+
+	for _, a := range analysis {
+		fmt.Println(a.Id, a.Name, a.IsChecked)
+	}
+
+	server.LaunchServer()
 }
--- pkg/migration/migration.go
+++ pkg/migration/migration.go
@@ -2,13 +2,26 @@ package migrations
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 
 	"github.com/pressly/goose"
+
+	"github.com/JulianaOsi/medhelp/pkg/config"
 )
 
-func Run(db *sql.DB) {
+func run(db *sql.DB) error {
 	if err := goose.Up(db, "."); err != nil {
-		log.Fatalf("goose up failed: %v", err)
+		return fmt.Errorf("goose: up failed: %v", err)
 	}
+
+	return nil
+}
+
+func UpMigrations(conf *config.Config) error {
+	db, err := sql.Open("postgres", conf.DB.ToString())
+	if err != nil {
+		return fmt.Errorf("goose: failed to open DB: %v\n", err)
+	}
+
+	return run(db)
 }
