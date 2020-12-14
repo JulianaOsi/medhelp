528c90585e8c823293e18bd254921494aa920c15
hacksocto <hacksocto@gmail.com>
2020-12-14 13:55:12 +0500

    Правки ошибок, чистка кода

--- pkg/store/analysis.go
+++ pkg/store/analysis.go
@@ -49,6 +49,43 @@ func (s *Store) GetAnalysisByDirectionId(ctx context.Context, directionId int) (
 	return analysis, nil
 }
 
+func (s *Store) GetAnalysisById(ctx context.Context, id int) (*Analysis, error) {
+	sql, _, err := goqu.Select("direction_analysis.id", "name", "is_checked", "file_id").
+		From("direction_analysis").
+		Where(goqu.L("\"direction_analysis\".\"id\"").Eq(id)).
+		LeftJoin(
+			goqu.T("analysis"),
+			goqu.On(goqu.Ex{
+				"analysis.id": goqu.I("direction_analysis.analysis_id"),
+			}),
+		).
+		ToSQL()
+	if err != nil {
+		return nil, fmt.Errorf("sql query build failed: %v", err)
+	}
+
+	rows, err := s.connPool.Query(ctx, sql)
+	if err != nil {
+		return nil, fmt.Errorf("execute a query failed: %v", err)
+	}
+	defer rows.Close()
+
+	var analysis []*Analysis
+
+	for rows.Next() {
+		newAnalysis, err := readAnalysis(rows)
+		if err != nil {
+			return nil, fmt.Errorf("read analysis failed: %v", err)
+		}
+		analysis = append(analysis, newAnalysis)
+	}
+
+	if len(analysis) == 0 {
+		return nil, nil
+	}
+	return analysis[0], nil
+}
+
 func (s *Store) SetAnalysisState(ctx context.Context, analysisId int, isChecked bool) error {
 	sql, _, err := goqu.Update("direction_analysis").
 		Set(goqu.Record{"is_checked": isChecked}).
